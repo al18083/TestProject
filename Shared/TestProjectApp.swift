b2020caeef5d673b3d97cb1c6777b8b7f456f6da
al18083 <al18083@shibaura-it.ac.jp>
2022-06-27 20:43:53 +0900

    Initial Commit

---
+++ Shared/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  Shared
+//
+//  Created by 中村友哉 on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
